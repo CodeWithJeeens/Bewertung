36af6ad1cc36663be6aa38da7e72c627ba37834a
CodeWithJeeens <69648341+CodeWithJeeens@users.noreply.github.com>
2022-02-27 22:46:15 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 const hamburger = documet.querySelector('.hamburger');
 
 hamburger.addEventListener('click', function (){
-    this.classList.toggle(is-active;)
+    this.classList.toggle(is-active);
 });
