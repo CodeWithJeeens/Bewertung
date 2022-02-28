94cc8acf83f8c744df44d3d96d85995c6c8de174
CodeWithJeeens <69648341+CodeWithJeeens@users.noreply.github.com>
2022-03-01 00:37:52 +0100

    Viele Fixes

--- script/main.js
+++ script/main.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 
+const link =document.querySelector('.link');
+
 hamburger.addEventListener('click', function (){
     hamburger.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+});
+
+link.addEventListener('click', function(){
+  hamburger.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
 });
